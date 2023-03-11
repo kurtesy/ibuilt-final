fb41056cdb2c4e5a83c73b9ec5bdc1eed53131a1
vinodkotagiri <vinodkotagiri@icloud.com>
2023-03-11 21:31:15 +0530

    added sample svg for balcony

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,5 @@
-// Svg images for all components
+import { balcony_flooring_list } from './svg/balcony/index.js'
+const components = {
+  balcony_flooring_list
+}
+export { components }
---
+++ src/assets/svg/balcony/index.js
@@ -0,0 +1,49 @@
+import balcony_flooring_black_granite_diamond from './balcony_flooring_black_granite_diamond.svg'
+import balcony_flooring_chess from './balcony_flooring_chess.svg'
+import balcony_flooring_diamonds from './balcony_flooring_diamonds.svg'
+import balcony_flooring_green_checks from './balcony_flooring_green_checks.svg'
+import balcony_flooring_marble_golden_white from './balcony_flooring_marble_golden_white.svg'
+import balcony_flooring_multi_coloured from './balcony_flooring_multi_coloured.svg'
+import balcony_flooring_orange_checks from './balcony_flooring_orange_checks.svg'
+import balcony_flooring_outdoor_blue_checks from './balcony_flooring_outdoor_blue_checks.svg'
+import balcony_flooring_pink_flower_marble from './balcony_flooring_pink_flower_marble.svg'
+import balcony_flooring_pink_marble_chess from './balcony_flooring_pink_marble_chess.svg'
+import balcony_flooring_pink_marble_with_flower from './balcony_flooring_pink_marble_with_flower.svg'
+import balcony_flooring_rubble_Stone from './balcony_flooring_rubble_Stone.svg'
+import balcony_flooring_slate from './balcony_flooring_slate.svg'
+import balcony_flooring_wood_variant_1 from './balcony_flooring_wood_variant_1.svg'
+import balcony_flooring_wood_variant_2 from './balcony_flooring_wood_variant_2.svg'
+import balcony_flooring_wood_variant_3 from './balcony_flooring_wood_variant_3.svg'
+import balcony_flooring_wood_variant_4 from './balcony_flooring_wood_variant_4.svg'
+import balcony_flooring_wood_variant_5 from './balcony_flooring_wood_variant_5.svg'
+import balcony_flooring_wood_variant_6 from './balcony_flooring_wood_variant_6.svg'
+import balcony_flooring_wood_variant_7 from './balcony_flooring_wood_variant_7.svg'
+import balcony_flooring_wood_variant_8 from './balcony_flooring_wood_variant_8.svg'
+import balcony_flooring_wood_variant_9 from './balcony_flooring_wood_variant_9.svg'
+import balcony_flooring_yellow_diamond from './balcony_flooring_yellow_diamond.svg'
+
+export const balcony_flooring_list = [
+  { id: 1, icon: balcony_flooring_black_granite_diamond },
+  { id: 2, icon: balcony_flooring_chess },
+  { id: 3, icon: balcony_flooring_diamonds },
+  { id: 4, icon: balcony_flooring_green_checks },
+  { id: 5, icon: balcony_flooring_marble_golden_white },
+  { id: 6, icon: balcony_flooring_multi_coloured },
+  { id: 7, icon: balcony_flooring_orange_checks },
+  { id: 8, icon: balcony_flooring_outdoor_blue_checks },
+  { id: 9, icon: balcony_flooring_pink_flower_marble },
+  { id: 10, icon: balcony_flooring_pink_marble_chess },
+  { id: 11, icon: balcony_flooring_pink_marble_with_flower },
+  { id: 12, icon: balcony_flooring_rubble_Stone },
+  { id: 13, icon: balcony_flooring_slate },
+  { id: 14, icon: balcony_flooring_wood_variant_1 },
+  { id: 15, icon: balcony_flooring_wood_variant_2 },
+  { id: 16, icon: balcony_flooring_wood_variant_3 },
+  { id: 17, icon: balcony_flooring_wood_variant_4 },
+  { id: 18, icon: balcony_flooring_wood_variant_5 },
+  { id: 19, icon: balcony_flooring_wood_variant_6 },
+  { id: 20, icon: balcony_flooring_wood_variant_7 },
+  { id: 21, icon: balcony_flooring_wood_variant_8 },
+  { id: 22, icon: balcony_flooring_wood_variant_9 },
+  { id: 23, icon: balcony_flooring_yellow_diamond }
+]
