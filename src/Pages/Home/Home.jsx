9944302f71809342232e69d763cabe9bea8cdb84
vinod kotagiri <vinodkotagiri@icloud.com>
2023-06-15 23:00:52 +0530

    added color

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -11,9 +11,9 @@ export default function Home() {
       <section className='w-full h-full bg-white'>
         <div className='w-full h-full flex items-center justify-around px-6 gap-3'>
           <div className='flex items-start px-24 flex-col gap-4  w-1/2'>
-            <div className='text-3xl text-slate-200  font-bold'>Build yourself a home with iBuiltup</div>
+            <div className='text-3xl text-slate-800  font-bold'>Build yourself a home with iBuiltup</div>
             <div className='text-primaryLime text-3xl pb-6  font-bold'>Build your Drawing</div>
-            <div className='text-lg text-slate-200'>iBuiltup is committed to empower you with simplified technical skills, therefore carve out your dream home project by yourself.</div>
+            <div className='text-lg text-slate-800'>iBuiltup is committed to empower you with simplified technical skills, therefore carve out your dream home project by yourself.</div>
             <Link
               to='/drawing'
               className='bg-primaryLime px-6 py-3 my-10 flex items-center justify-around gap-4 font-semibold rounded-md text-2xl hover:scale-110 ease-linear duration-300 cursor-pointer'>
@@ -21,7 +21,7 @@ export default function Home() {
               <BsFillArrowRightCircleFill size={24} />
             </Link>
           </div>
-          <div className='w-1/2 flex items-center justify-center font-thin bg-slate-200 h-[600px]'>YOUR VIDEO</div>
+          <div className='w-1/2 flex items-center justify-center font-thin bg-slate-800 h-[600px]'>YOUR VIDEO</div>
         </div>
       </section>
     </div>
